5912ac9fd53a587e558fc60f4a4d98741cb6b26d
Bakanier <kushnazaruul@gmail.com>
2023-10-17 12:16:56 +0600

    Home work :3

---
+++ main.js
@@ -0,0 +1,70 @@
+const person = {
+    name:"Beka",
+    age: 21 ,
+    city: "Bishkek"
+}
+
+person.age = 23
+
+console.log(person);
+
+const numbers = [1, 2, 3, 4, 5];
+
+const length = numbers.length;
+console.log("Длина массива numbers: " + length);
+
+const animals = [
+    {
+        animal:"Eagle"
+    },
+    {
+        animal:"Lion"
+    },
+    {
+        animal:"Crab"
+    },
+    {
+        animal:"Wolf"
+    }
+]
+
+console.log(animals);
+
+
+const people = [
+    {
+        name:"Улик",
+        age: 22 ,
+        city:"Майами"
+    },
+
+    {
+        name:"Амир" ,
+        age: 27 ,
+        city:"Костанай"
+    },
+    {
+        name:"Бека",
+        age: 21 ,
+        city:"Бишкек"
+
+    }
+]
+
+console.log(people);
+
+const data = [42, "Gain more experience before you challenge me.", { name: "Объект", type: "Тип данных" }];
+
+console.log(data);
+
+const fruits = ["Яблоко", "Банан", "Апельсин", "Груша", "Вишня"];
+
+console.log(fruits[2]);
+
+const book = {
+    title: "Крысы в стенах",
+    author: "Говард Лавкрафт",
+    year: 1924
+};
+
+console.log("Автор книги: " + book.author);
